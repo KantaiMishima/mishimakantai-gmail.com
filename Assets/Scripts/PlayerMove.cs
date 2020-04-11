0fa8c2a9862fcdb198f53ae18b78328dbc95b8d1
KantaiMishima <mishimakantai@gmail.com>
2020-04-11 17:17:40 +0900

    create a varsion

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerStage;
+    [SerializeField]
+    private float angularVelocity = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angular = - angularVelocity * Mathf.Sin(this.transform.localRotation.y);
+        playerStage.transform.Rotate(new Vector3(0, angular, 0));
+    }
+}
---
+++ Assets/Scripts/Stone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stone : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rb = this.GetComponent<Rigidbody>();
+        rb.velocity = speed * this.transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") {
+            SceneManager.LoadScene("gameOver");
+        }
+    }
+}
