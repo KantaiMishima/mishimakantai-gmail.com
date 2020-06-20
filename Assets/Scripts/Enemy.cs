a2f33208c8dc37b2efbfa9eb575b5347dd7d1975
KantaiMishima <mishimakantai@gmail.com>
2020-06-20 15:17:46 +0900

    update エネミーのダメージ

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int initialHP = 100;
+    int HP = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP = initialHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet") {
+            HP--;
+            Destroy(other.gameObject);
+            if (HP <= 0) {
+                SceneManager.LoadScene("gameClear");
+            }
+        }
+    }
+}
